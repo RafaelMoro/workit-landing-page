a12969f2a1da591ac29f1bbf78f8eacc4a6d1abb
Rafael Moro <rafael.moro.galindo@gmail.com>
2023-11-26 12:08:35 -0600

    [ADD] - Adding fonts

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>Something</p>
+        </div>
+        <div>
+          <h2>Prueba de font</h2>
+        </div>
       </header>
     </div>
   );
